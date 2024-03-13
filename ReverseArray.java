de8e3914598cd5b6f0c23cacfdce25976a28b266
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-13 21:59:22 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class ReverseArrayExample {
+public class ReverseArray {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
         reverseArray(array);
